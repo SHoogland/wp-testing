7cf0f19325c69e6b88d1a6fcc4e30bee7cd9265f
Stephan Hoogland <stephan@shoogland.com>
2020-04-15 22:18:40 +0200

    try pupetteer

---
+++ index.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer-core')
+
+(async () => {
+	const browser = await puppeteer.launch({executablePath: process.env.CHROME_BIN})
+	const page = await browser.newPage()
+	await page.goto('http://localhost', {waitUntil: 'networkidle2'})
+	let content = await page.content()
+	console.log(content)
+
+	await browser.close()
+})()
