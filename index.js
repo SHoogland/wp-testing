95283129311931cef74bce50828c51b28fd263e5
Stephan Hoogland <stephan@shoogland.com>
2020-04-16 10:47:32 +0200

    make 2 screenshots

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const puppeteer = require('puppeteer-core');
 	const browser = await puppeteer.launch({executablePath: process.env.CHROME_BIN})
 	const page = await browser.newPage()
 	await page.goto('http://localhost:8888', {waitUntil: 'networkidle2'})
-	await page.screenshot({path: 'screenshot.png'});
+	await page.screenshot({path: 'screenshots/screenshot.png'});
+	await page.goto('http://localhost:8888/wp-admin', {waitUntil: 'networkidle2'})
+	await page.screenshot({path: 'screenshots/screenshot2.png'});
 	await browser.close()
 })()
